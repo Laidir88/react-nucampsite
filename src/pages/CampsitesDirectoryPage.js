72a7d96e85c376dd7850488056e1e109543c5dd0
tovawhit <tovah_whitesell@selinc.com>
2022-07-20 15:21:32 -0500

    hooks useState added for campsite selection

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,16 +1,18 @@
-import { Container, Row, Col, Button } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import CampsiteDetail from "../features/campsites/CampsiteDetail";
 import CampsiteList from "../features/campsites/CampsiteList";
-import { selectRandomCampsite } from "../features/campsites/campsitesSlice";
+import { selectCampsiteById } from "../features/campsites/campsitesSlice";
+import { useState } from "react";
 
 const CampsitesDirectoryPage = () => {
-  const selectedCampsite = selectRandomCampsite();
+  const [campsiteId, setCampsiteId] = useState(0);
+  const selectedCampsite = selectCampsiteById(campsiteId);
 
   return (
     <Container>
       <Row>
         <Col sm="5" md="7">
-          <CampsiteList />
+          <CampsiteList setCampsiteId={setCampsiteId} />
         </Col>
         <Col sm="7" md="5">
           <CampsiteDetail campsite={selectedCampsite} />
