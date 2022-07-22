45b7582be72bd292c166ebcfec5bba49b7a7cb4d
Laidir88 <ktbriana@gmail.com>
2022-07-22 14:52:26 -0500

    Made star ratings icons rather than nums

--- src/features/comments/Comment.js
+++ src/features/comments/Comment.js
@@ -2,11 +2,22 @@ import { formatDate } from "../../utils/formatDate";
 
 const Comment = ({ comment }) => {
   const { text: commentText, rating, author, date } = comment;
+  // Let's make the star ratings icons rather than a number
+  let star = [];
+  for (let i = 0; i < rating; i++) {
+    star.push(<i class="fa fa-star" aria-hidden="true"></i>);
+  }
+  let noStar = [];
+  for (let i = 0; i < 5 - rating; i++) {
+    noStar.push(<i class="fa fa-star-o" aria-hidden="true"></i>);
+  }
+
   return (
     <p>
       {commentText}
       <br />
-      {rating}/5 stars -- {author}, {formatDate(date)}
+      {star}
+      {noStar} -- {author}, {formatDate(date)}
     </p>
   );
 };
